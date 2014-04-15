7048c13ffab591dfb070f4b54eda195d3ee9e9e6
pmeems_cp <SND\pmeems_cp>
2014-04-15 09:31:11 -0700

    First showcase using Syncfusion toolbox.

---
+++ trunk/MapWindow/Forms/MainForm.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace MapWindow
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection ccbLegend = new Syncfusion.Windows.Forms.Tools.CaptionButtonsCollection();
+            Syncfusion.Windows.Forms.Tools.TreeNodeAdv treeNodeAdv1 = new Syncfusion.Windows.Forms.Tools.TreeNodeAdv();
+            this.dockingManager1 = new Syncfusion.Windows.Forms.Tools.DockingManager(this.components);
+            this.Legend = new Syncfusion.Windows.Forms.Tools.TreeViewAdv();
+            this.dockingClientPanel1 = new Syncfusion.Windows.Forms.Tools.DockingClientPanel();
+            this.axMap1 = new AxMapWinGIS.AxMap();
+            this.statusStripEx1 = new Syncfusion.Windows.Forms.Tools.StatusStripEx();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripPanelItem1 = new Syncfusion.Windows.Forms.Tools.ToolStripPanelItem();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripEx1 = new Syncfusion.Windows.Forms.Tools.ToolStripEx();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dockingManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Legend)).BeginInit();
+            this.dockingClientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axMap1)).BeginInit();
+            this.statusStripEx1.SuspendLayout();
+            this.toolStripEx1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dockingManager1
+            // 
+            this.dockingManager1.ActiveCaptionFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World);
+            this.dockingManager1.DockLayoutStream = ((System.IO.MemoryStream)(resources.GetObject("dockingManager1.DockLayoutStream")));
+            this.dockingManager1.DragProviderStyle = Syncfusion.Windows.Forms.Tools.DragProviderStyle.VS2012;
+            this.dockingManager1.HostControl = this;
+            this.dockingManager1.InActiveCaptionBackground = new Syncfusion.Drawing.BrushInfo(System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(211)))), ((int)(((byte)(212))))));
+            this.dockingManager1.InActiveCaptionFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World);
+            this.dockingManager1.MetroButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dockingManager1.MetroCaptionColor = System.Drawing.Color.White;
+            this.dockingManager1.MetroColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(165)))), ((int)(((byte)(220)))));
+            this.dockingManager1.MetroSplitterBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(159)))), ((int)(((byte)(183)))));
+            this.dockingManager1.ReduceFlickeringInRtl = false;
+            this.dockingManager1.VisualStyle = Syncfusion.Windows.Forms.VisualStyle.Metro;
+            this.dockingManager1.CaptionButtons.Add(new Syncfusion.Windows.Forms.Tools.CaptionButton(Syncfusion.Windows.Forms.Tools.CaptionButtonType.Close, "CloseButton"));
+            this.dockingManager1.CaptionButtons.Add(new Syncfusion.Windows.Forms.Tools.CaptionButton(Syncfusion.Windows.Forms.Tools.CaptionButtonType.Pin, "PinButton"));
+            this.dockingManager1.CaptionButtons.Add(new Syncfusion.Windows.Forms.Tools.CaptionButton(Syncfusion.Windows.Forms.Tools.CaptionButtonType.Menu, "MenuButton"));
+            this.dockingManager1.CaptionButtons.Add(new Syncfusion.Windows.Forms.Tools.CaptionButton(Syncfusion.Windows.Forms.Tools.CaptionButtonType.Maximize, "MaximizeButton"));
+            this.dockingManager1.CaptionButtons.Add(new Syncfusion.Windows.Forms.Tools.CaptionButton(Syncfusion.Windows.Forms.Tools.CaptionButtonType.Restore, "RestoreButton"));
+            this.dockingManager1.SetDockLabel(this.Legend, "Legend");
+            this.dockingManager1.SetEnableDocking(this.Legend, true);
+            ccbLegend.MergeWith(this.dockingManager1.CaptionButtons, false);
+            this.dockingManager1.SetCustomCaptionButtons(this.Legend, ccbLegend);
+            // 
+            // Legend
+            // 
+            this.Legend.BackColor = System.Drawing.Color.White;
+            this.Legend.BeforeTouchSize = new System.Drawing.Size(148, 557);
+            this.Legend.Border3DStyle = System.Windows.Forms.Border3DStyle.Flat;
+            this.Legend.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Legend.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Legend.CanSelectDisabledNode = false;
+            this.Legend.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            // 
+            // 
+            // 
+            this.Legend.HelpTextControl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Legend.HelpTextControl.Location = new System.Drawing.Point(0, 0);
+            this.Legend.HelpTextControl.Name = "helpText";
+            this.Legend.HelpTextControl.Size = new System.Drawing.Size(49, 15);
+            this.Legend.HelpTextControl.TabIndex = 0;
+            this.Legend.HelpTextControl.Text = "help text";
+            this.Legend.HotTracking = true;
+            this.Legend.ItemHeight = 24;
+            this.Legend.Location = new System.Drawing.Point(1, 19);
+            this.Legend.MetroColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(165)))), ((int)(((byte)(220)))));
+            this.Legend.MetroScrollBars = true;
+            this.Legend.Name = "Legend";
+            treeNodeAdv1.CheckState = System.Windows.Forms.CheckState.Checked;
+            treeNodeAdv1.ChildStyle.EnsureDefaultOptionedChild = true;
+            treeNodeAdv1.EnsureDefaultOptionedChild = true;
+            treeNodeAdv1.Expanded = true;
+            treeNodeAdv1.MultiLine = true;
+            treeNodeAdv1.PlusMinusSize = new System.Drawing.Size(9, 9);
+            treeNodeAdv1.ShowLine = true;
+            treeNodeAdv1.ShowOptionButton = false;
+            treeNodeAdv1.Text = "Layers";
+            this.Legend.Nodes.AddRange(new Syncfusion.Windows.Forms.Tools.TreeNodeAdv[] {
+            treeNodeAdv1});
+            this.Legend.ScaleFactor = 1F;
+            this.Legend.SelectedNodeBackground = new Syncfusion.Drawing.BrushInfo(System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(165)))), ((int)(((byte)(220))))));
+            this.Legend.SelectedNodeForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            this.Legend.ShowCheckBoxes = true;
+            this.Legend.ShowFocusRect = false;
+            this.Legend.Size = new System.Drawing.Size(148, 557);
+            this.Legend.Style = Syncfusion.Windows.Forms.Tools.TreeStyle.Metro;
+            this.Legend.TabIndex = 3;
+            this.Legend.Text = "treeViewAdv1";
+            // 
+            // 
+            // 
+            this.Legend.ToolTipControl.BackColor = System.Drawing.SystemColors.Info;
+            this.Legend.ToolTipControl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Legend.ToolTipControl.Location = new System.Drawing.Point(0, 0);
+            this.Legend.ToolTipControl.Name = "toolTip";
+            this.Legend.ToolTipControl.Size = new System.Drawing.Size(41, 15);
+            this.Legend.ToolTipControl.TabIndex = 1;
+            this.Legend.ToolTipControl.Text = "toolTip";
+            this.Legend.TransparentControls = true;
+            // 
+            // dockingClientPanel1
+            // 
+            this.dockingClientPanel1.Controls.Add(this.axMap1);
+            this.dockingClientPanel1.Location = new System.Drawing.Point(155, 59);
+            this.dockingClientPanel1.Name = "dockingClientPanel1";
+            this.dockingClientPanel1.Size = new System.Drawing.Size(560, 456);
+            this.dockingClientPanel1.TabIndex = 0;
+            // 
+            // axMap1
+            // 
+            this.axMap1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axMap1.Enabled = true;
+            this.axMap1.Location = new System.Drawing.Point(0, 0);
+            this.axMap1.Name = "axMap1";
+            this.axMap1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMap1.OcxState")));
+            this.axMap1.Size = new System.Drawing.Size(560, 456);
+            this.axMap1.TabIndex = 0;
+            this.axMap1.FileDropped += new AxMapWinGIS._DMapEvents_FileDroppedEventHandler(this.axMap1_FileDropped);
+            // 
+            // statusStripEx1
+            // 
+            this.statusStripEx1.AutoSize = false;
+            this.statusStripEx1.BeforeTouchSize = new System.Drawing.Size(893, 22);
+            this.statusStripEx1.Dock = Syncfusion.Windows.Forms.Tools.DockStyleEx.Bottom;
+            this.statusStripEx1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripPanelItem1,
+            this.toolStripProgressBar1});
+            this.statusStripEx1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Flow;
+            this.statusStripEx1.Location = new System.Drawing.Point(0, 623);
+            this.statusStripEx1.MetroColor = System.Drawing.Color.Empty;
+            this.statusStripEx1.Name = "statusStripEx1";
+            this.statusStripEx1.Size = new System.Drawing.Size(893, 22);
+            this.statusStripEx1.TabIndex = 1;
+            this.statusStripEx1.Text = "statusStripEx1";
+            this.statusStripEx1.VisualStyle = Syncfusion.Windows.Forms.Tools.StatusStripExStyle.Metro;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(90, 15);
+            this.toolStripStatusLabel1.Text = "MapWindow v5";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(90, 15);
+            this.toolStripStatusLabel2.Text = "Some other text";
+            // 
+            // toolStripPanelItem1
+            // 
+            this.toolStripPanelItem1.AutoSize = false;
+            this.toolStripPanelItem1.CausesValidation = false;
+            this.toolStripPanelItem1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.toolStripPanelItem1.Name = "toolStripPanelItem1";
+            this.toolStripPanelItem1.Size = new System.Drawing.Size(23, 23);
+            this.toolStripPanelItem1.Text = "toolStripPanelItem1";
+            this.toolStripPanelItem1.Transparent = true;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 15);
+            // 
+            // toolStripEx1
+            // 
+            this.toolStripEx1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.toolStripEx1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStripEx1.Image = null;
+            this.toolStripEx1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStripEx1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStripEx1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripEx1.Name = "toolStripEx1";
+            this.toolStripEx1.Office12Mode = false;
+            this.toolStripEx1.ShowCaption = false;
+            this.toolStripEx1.ShowLauncher = false;
+            this.toolStripEx1.Size = new System.Drawing.Size(893, 46);
+            this.toolStripEx1.TabIndex = 2;
+            this.toolStripEx1.VisualStyle = Syncfusion.Windows.Forms.Tools.ToolStripExStyle.Metro;
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(38, 43);
+            this.toolStripButton1.Text = "Clear";
+            this.toolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.toolStripButton1.ToolTipText = "Click me";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderThickness = 2;
+            this.CaptionBarColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(158)))), ((int)(((byte)(218)))));
+            this.CaptionBarHeight = 24;
+            this.CaptionFont = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClientSize = new System.Drawing.Size(893, 645);
+            this.Controls.Add(this.toolStripEx1);
+            this.Controls.Add(this.statusStripEx1);
+            this.Controls.Add(this.dockingClientPanel1);
+            this.DoubleBuffered = true;
+            this.DropShadow = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "MapWindow Open Source GIS v5";
+            ((System.ComponentModel.ISupportInitialize)(this.dockingManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Legend)).EndInit();
+            this.dockingClientPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axMap1)).EndInit();
+            this.statusStripEx1.ResumeLayout(false);
+            this.statusStripEx1.PerformLayout();
+            this.toolStripEx1.ResumeLayout(false);
+            this.toolStripEx1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.Tools.DockingManager dockingManager1;
+        private Syncfusion.Windows.Forms.Tools.DockingClientPanel dockingClientPanel1;
+        private Syncfusion.Windows.Forms.Tools.ToolStripEx toolStripEx1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private Syncfusion.Windows.Forms.Tools.StatusStripEx statusStripEx1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private Syncfusion.Windows.Forms.Tools.ToolStripPanelItem toolStripPanelItem1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private Syncfusion.Windows.Forms.Tools.TreeViewAdv Legend;
+        private AxMapWinGIS.AxMap axMap1;
+    }
+}
+
---
+++ trunk/MapWindow/Forms/MainForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Syncfusion.Windows.Forms;
+using Syncfusion.Windows.Forms.Tools;
+
+namespace MapWindow
+{
+    using System.IO;
+    using System.Runtime.Remoting.Messaging;
+
+    using MapWinGIS;
+
+    public partial class MainForm : Syncfusion.Windows.Forms.MetroForm, MapWinGIS.ICallback
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // TODO: Doesn't work:
+            //this.axMap1.GlobalCallback = this;
+
+            var gs = new GlobalSettingsClass { GridProxyFormat = tkGridProxyFormat.gpfTiffProxy };
+            this.toolStripProgressBar1.Minimum = 0;
+            this.toolStripProgressBar1.Maximum = 100;
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            this.axMap1.Clear();
+
+            // Clear legend
+            if (this.Legend.Nodes[0].Nodes.Count > 0)
+            {
+                for (var i = this.Legend.Nodes[0].Nodes.Count - 1; i >= 0; i--)
+                {
+                    this.Legend.Nodes[0].Nodes[i].Remove();
+                }
+            }
+        }
+
+        private void axMap1_FileDropped(object sender, AxMapWinGIS._DMapEvents_FileDroppedEvent e)
+        {
+            this.toolStripProgressBar1.Value = 0;
+
+            // TODO use async:
+            var hndl = this.axMap1.AddLayerFromFilename(e.filename, tkFileOpenStrategy.fosAutoDetect, true);
+            if (hndl == -1)
+            {
+                return;
+            }
+
+            // Check if a symbology file is present:
+            var symbFilename = e.filename + ".mwsymb";
+            if (File.Exists(symbFilename))
+            {
+                var layerDesc = string.Empty;
+                this.axMap1.LoadLayerOptions(hndl, string.Empty, ref layerDesc);
+            }
+
+            // Redraw map:
+            this.axMap1.Redraw2(tkRedrawType.RedrawAll);
+
+            // Add t0 legend:
+            var nodeName = this.axMap1.get_LayerName(hndl);
+            if (nodeName == string.Empty)
+            {
+                nodeName = Path.GetFileNameWithoutExtension(this.axMap1.get_LayerFilename(hndl));
+            }
+
+            var newNode = new TreeNodeAdv(nodeName) { CheckState = CheckState.Checked };
+
+            this.Legend.Nodes[0].Nodes.Insert(0, newNode);
+        }
+
+        public void Error(string KeyOfSender, string ErrorMsg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Progress(string KeyOfSender, int Percent, string Message)
+        {
+            this.toolStripProgressBar1.Value = Percent;
+        }
+    }
+}
