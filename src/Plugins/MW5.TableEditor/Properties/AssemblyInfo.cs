106f3388c46f8162a222d4a5bb28b5c628c05957
sleschinski <lsu@mail.by>
2015-03-25 11:53:57 +0300

    Removing Copy local flag from plugin dependencies.

--- src/Plugins/MW5.TableEditor/Properties/AssemblyInfo.cs
+++ src/Plugins/MW5.TableEditor/Properties/AssemblyInfo.cs
@@ -1,3 +1,12 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="MapWindow OSS Team - www.mapwindow.org">
+//   MapWindow OSS Team - 2015
+// </copyright>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -5,32 +14,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TableEditorNew")]
+[assembly: AssemblyTitle("MW5.TableEditor")]
+[assembly: AssemblyProduct("Table editor")]
 [assembly: AssemblyDescription("New mapWindow plug-in for editing shapefile attribute tables.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MapWindow OSS Team - www.mapwindow.org")]
-[assembly: AssemblyProduct("TableEditorNew")]
-[assembly: AssemblyCopyright("Copyright (C) 2012 MapWindow OSS Team")]
-[assembly: AssemblyTrademark("MapWindow GIS is a trademark of Daniel P. Ames, 2005-2012")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("MapWindow OSS Team")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("29b58add-b147-4ba2-9d04-218701be9e9d")]
+[assembly: Guid("894E958F-69DD-48DF-B35B-16871EC5D309")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.*")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
