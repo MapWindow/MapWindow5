608921154221bb3c8ab3d26b9053601343ff1aee
sleschinski <lsu@mail.by>
2015-05-03 17:09:03 +0300

    PostBuild events for release configuration: a) copying MapWinGIS binaries and dependencies; b) removing ocx file copied by build process (we want it in subfolder); c) changing reference in manifest file from mapwingis.ocx to mapwingis\mapwingis.ocx.

---
+++ src/MW5.PostBuild/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace MW5.PostBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UpdateManifest();
+
+            RemoveOcx();
+        }
+
+        private static void RemoveOcx()
+        {
+            File.Delete("mapwingis.ocx");
+            File.Delete("plugins\\mapwingis.ocx");
+        }
+
+        private static void UpdateManifest()
+        {
+            const string path = "Native.MW5.Api.manifest";
+            const string oldFilename = "name=\"MapWinGIS.ocx\"";
+            const string newFilename = "name=\"MapWinGis\\MapWinGIS.ocx\"";
+
+            var s = File.ReadAllText(path);
+            if (!string.IsNullOrWhiteSpace(s))
+            {
+                File.WriteAllText(path, s.Replace(oldFilename, newFilename));
+                Console.WriteLine("Text was replaced");
+            }
+        }
+    }
+}
---
+++ src/postbuild.bat
@@ -0,0 +1,2 @@
+xcopy d:\mw\MapWinGis\src\bin\Win32\* d:\mw\mw5\src\bin\release\mapwingis\* /Y /R /s
+MW5.PostBuild.exe
