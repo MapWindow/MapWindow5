1d026b309ed1f9ee613306b8e5d090bc9e970d49
sleschinski <lsu@mail.by>
2015-09-06 11:39:12 +0300

    Minor changes of project references while testing release configuration.

--- src/postbuild.bat
+++ src/postbuild.bat
@@ -1,2 +1,2 @@
-xcopy d:\mw\MapWinGis\src\bin\Win32\* d:\mw\mw5\src\bin\release\mapwingis\* /Y /R /s
+xcopy d:\mw\MapWinGis\src\bin\Win32\* d:\mw\mw5\src\bin\release\MapWinGIS\* /Y /R /s
 MW5.PostBuild.exe
