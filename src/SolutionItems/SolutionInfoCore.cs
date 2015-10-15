c49bec54421c7cdabfc028b6f8bc5f1953e2b4a3
Paul Meems <p.meems@topx-group.nl>
2015-10-15 21:03:46 +0200

    Update version number since we already published v5.0.1

--- src/SolutionItems/SolutionInfoCore.cs
+++ src/SolutionItems/SolutionInfoCore.cs
@@ -18,13 +18,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (C) 1998-2015 MapWindow OSS Team")]
 [assembly: AssemblyTrademark("MapWindow GIS is a trademark of Daniel P. Ames, 2005-2015")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyTitle("MapWindow v5 Core binaries")]
-[assembly: AssemblyDescription("The core binaries for MapWindow v5")]
+[assembly: AssemblyTitle("MapWindow5 GIS")]
+[assembly: AssemblyDescription("MapWindow5 Open Source GIS application")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("5.0.1.*")]
-[assembly: AssemblyFileVersion("5.0.1.0")]
+[assembly: AssemblyVersion("5.0.2.*")]
+[assembly: AssemblyFileVersion("5.0.2.0")]
