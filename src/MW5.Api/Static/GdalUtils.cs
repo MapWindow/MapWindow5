0ce77b944802b05dde3e62a18813f1b95862e312
Paul Meems <p.meems@akkerweb.nl>
2017-07-06 12:12:21 +0200

    Implemented new GdalWarp implementation from MapWinGIS, made in MWGIS-34: Implement GDALWarp function from GDAL

--- src/MW5.Api/Static/GdalUtils.cs
+++ src/MW5.Api/Static/GdalUtils.cs
@@ -4,6 +4,7 @@
 // </copyright>
 // -------------------------------------------------------------------------------------------
 
+using System;
 using MapWinGIS;
 using MW5.Api.Concrete;
 using MW5.Api.Interfaces;
@@ -13,8 +14,10 @@ namespace MW5.Api.Static
 {
     public class GdalUtils
     {
-        private static readonly GdalUtils _staticUilts = new GdalUtils();
+        private static readonly GdalUtils _staticUtils = new GdalUtils();
         private readonly Utils _utils = new Utils();
+        private readonly MapWinGIS.GdalUtils _mwgisGdalUtils = new MapWinGIS.GdalUtils();
+        private string _lastErrorMessage;
 
         public IGlobalListener Callback
         {
@@ -23,6 +26,8 @@ namespace MW5.Api.Static
             {
                 var callback = NativeCallback.Wrap(value);
                 _utils.GlobalCallback = callback;
+
+                // TODO: Use with _mwgisGdalUtils as well
 
                 // TODO: implement
                 //_utils.StopExecution = callback;
@@ -35,7 +40,7 @@ namespace MW5.Api.Static
         /// </summary>
         public static GdalUtils Instance
         {
-            get { return _staticUilts; }
+            get { return _staticUtils; }
         }
 
         public bool GdalAddOverviews(string bstrSrcFilename, string bstrOptions, string bstrLevels)
@@ -73,15 +78,47 @@ namespace MW5.Api.Static
             return _utils.TranslateRaster(srcFilename, dstFilename, bstrOptions);
         }
 
+        [Obsolete("Use WarpRaster(string srcFilename, string dstFilename, string[] options)")]
         public bool WarpRaster(string srcFilename, string dstFilename, ISpatialReference newProjection)
         {
             string options = string.Format("-t_srs \"{0}\"", newProjection.ExportToProj4());
             return _utils.GDALWarp(srcFilename, dstFilename, options);
         }
 
+        [Obsolete("Use WarpRaster(string srcFilename, string dstFilename, string[] options)")]
         public bool WarpRaster(string srcFilename, string dstFilename, string options)
         {
-            return _utils.GDALWarp(srcFilename, dstFilename, options);
+            var retVal = _utils.GDALWarp(srcFilename, dstFilename, options);
+            if (!retVal)
+            {
+                _lastErrorMessage = _utils.ErrorMsg[_utils.LastErrorCode];
+            }
+
+            return retVal;
+        }
+
+
+        /// <summary>
+        /// Warps the raster. Using Gdal.GDALWarp (available since GDALv2)
+        /// </summary>
+        /// <param name="srcFilename">The source filename.</param>
+        /// <param name="dstFilename">The DST filename.</param>
+        /// <param name="options">The options. See http://www.gdal.org/gdalwarp.html for the possible options.</param>
+        /// <returns>True on success</returns>
+        public bool WarpRaster(string srcFilename, string dstFilename, string[] options)
+        {
+            var retVal = _mwgisGdalUtils.GDALWarp(srcFilename, dstFilename, options);
+            if (!retVal)
+            {
+                _lastErrorMessage = _mwgisGdalUtils.ErrorMsg[_mwgisGdalUtils.LastErrorCode];
+            }
+
+            return retVal;
+        }
+
+        public string LastErrorMessage()
+        {
+            return _lastErrorMessage;
         }
     }
 }
